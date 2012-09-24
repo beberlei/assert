97dbb186aeefec0ac6c9dd06fac9fc0e5efb3706
Nino <email@sharpner.de>
2012-09-24 21:07:32 +0200

    weak boolean check instead of type safe comparison operator

--- lib/Assert/Assertion.php
+++ lib/Assert/Assertion.php
@@ -658,7 +658,7 @@ class Assertion
      */
     static public function alnum($value, $message = null, $propertyPath = null)
     {
-        if (ctype_alnum($value) !== true) {
+        if ( ! ctype_alnum($value)) {
                throw static::createException($message, static::INVALID_ALNUM, $propertyPath);
         }
     }
