08cacf824ee6ebde2feac3ce642f9d95ed63181f
Bastian Feder <github@bastian-feder.de>
2012-10-19 18:00:07 +0200

    Implemented: added ability to assert the implementation of a specific interface

--- lib/Assert/Assertion.php
+++ lib/Assert/Assertion.php
@@ -20,40 +20,41 @@ namespace Assert;
  */
 class Assertion
 {
-    const INVALID_INTEGER         = 10;
-    const INVALID_DIGIT           = 11;
-    const INVALID_INTEGERISH      = 12;
-    const INVALID_BOOLEAN         = 13;
-    const VALUE_EMPTY             = 14;
-    const VALUE_NULL              = 15;
-    const INVALID_STRING          = 16;
-    const INVALID_REGEX           = 17;
-    const INVALID_MIN_LENGTH      = 18;
-    const INVALID_MAX_LENGTH      = 19;
-    const INVALID_STRING_START    = 20;
-    const INVALID_STRING_CONTAINS = 21;
-    const INVALID_CHOICE          = 22;
-    const INVALID_NUMERIC         = 23;
-    const INVALID_ARRAY           = 24;
-    const INVALID_KEY_EXISTS      = 26;
-    const INVALID_NOT_BLANK       = 27;
-    const INVALID_INSTANCE_OF     = 28;
-    const INVALID_SUBCLASS_OF     = 29;
-    const INVALID_RANGE           = 30;
-    const INVALID_ALNUM           = 31;
-    const INVALID_TRUE            = 32;
-    const INVALID_EQ              = 33;
-    const INVALID_SAME            = 34;
-    const INVALID_MIN             = 35;
-    const INVALID_MAX             = 36;
-    const INVALID_LENGTH          = 37;
-    const INVALID_FALSE           = 38;
-    const INVALID_DIRECTORY       = 101;
-    const INVALID_FILE            = 102;
-    const INVALID_READABLE        = 103;
-    const INVALID_WRITEABLE       = 104;
-    const INVALID_CLASS           = 105;
-    const INVALID_EMAIL           = 201;
+    const INVALID_INTEGER           = 10;
+    const INVALID_DIGIT             = 11;
+    const INVALID_INTEGERISH        = 12;
+    const INVALID_BOOLEAN           = 13;
+    const VALUE_EMPTY               = 14;
+    const VALUE_NULL                = 15;
+    const INVALID_STRING            = 16;
+    const INVALID_REGEX             = 17;
+    const INVALID_MIN_LENGTH        = 18;
+    const INVALID_MAX_LENGTH        = 19;
+    const INVALID_STRING_START      = 20;
+    const INVALID_STRING_CONTAINS   = 21;
+    const INVALID_CHOICE            = 22;
+    const INVALID_NUMERIC           = 23;
+    const INVALID_ARRAY             = 24;
+    const INVALID_KEY_EXISTS        = 26;
+    const INVALID_NOT_BLANK         = 27;
+    const INVALID_INSTANCE_OF       = 28;
+    const INVALID_SUBCLASS_OF       = 29;
+    const INVALID_RANGE             = 30;
+    const INVALID_ALNUM             = 31;
+    const INVALID_TRUE              = 32;
+    const INVALID_EQ                = 33;
+    const INVALID_SAME              = 34;
+    const INVALID_MIN               = 35;
+    const INVALID_MAX               = 36;
+    const INVALID_LENGTH            = 37;
+    const INVALID_FALSE             = 38;
+    const INVALID_DIRECTORY         = 101;
+    const INVALID_FILE              = 102;
+    const INVALID_READABLE          = 103;
+    const INVALID_WRITEABLE         = 104;
+    const INVALID_CLASS             = 105;
+    const INVALID_EMAIL             = 201;
+    const INTERFACE_NOT_IMPLEMENTED = 202;
 
     /**
      * Exception to throw when an assertion failed.
@@ -710,6 +711,23 @@ class Assertion
     {
         if ( ! class_exists($value)) {
             throw static::createException($message, static::INVALID_CLASS, $propertyPath);
+        }
+    }
+
+    /**
+     * Assert that the class implements the interface
+     *
+     * @param mixed $class
+     * @param string $interfaceName
+     * @param string $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    static public function implementsInterface($class, $interfaceName, $message = null, $propertyPath = null)
+    {
+        $reflection = new \ReflectionClass($class);
+        if ( ! $reflection->implementsInterface($interfaceName)) {
+            throw static::createException($message, static::INTERFACE_NOT_IMPLEMENTED, $propertyPath);
         }
     }
 
