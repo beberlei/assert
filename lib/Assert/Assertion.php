aea7aab483503d36cc2071fb6d7ddad7d5165769
Mathieu Rochette <mathieu@rochette.cc>
2015-06-24 23:29:50 +0200

    fix Assertion::count error message

--- lib/Assert/Assertion.php
+++ lib/Assert/Assertion.php
@@ -1444,7 +1444,6 @@ class Assertion
         if ($count !== count($countable)) {
             $message = sprintf(
                 $message ?: 'List does not contain exactly "%d" elements.',
-                self::stringify($countable),
                 self::stringify($count)
             );
 
