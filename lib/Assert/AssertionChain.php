c31394df552defb931f1ace9ee7407d54c75de32
Andreas Möller <am@localheinz.com>
2016-12-15 13:53:11 -0500

    Fix: Run 'composer assert:cs-fix'

--- lib/Assert/AssertionChain.php
+++ lib/Assert/AssertionChain.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Assert
  *
@@ -8,7 +9,6 @@
  * with this package in the file LICENSE.txt.
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- *
  * to kontakt@beberlei.de so I can send you a copy immediately.
  */
 
